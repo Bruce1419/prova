e3f7bc3464082b47cd368854b52a1804bcf2c28e
Bruce1419 <bruce1419@gmail.com>
2016-07-29 22:54:53 +0200

    Update __init__.py

--- pogom/__init__.py
+++ pogom/__init__.py
@@ -5,4 +5,5 @@ config = {
     'LOCALE': 'en',
     'LOCALES_DIR': 'static/locales',
     'ROOT_PATH': None,
-    'GOOGLEMAPS_KEY': 'AIzaSyC2kzCIfH_mngkq-oX2ign7Xryj0xmP_Oc',
+    'GOOGLEMAPS_KEY': 'AIzaSyC2kzCIfH_mngkq-oX2ign7Xryj0xmP_Oc'
+}
